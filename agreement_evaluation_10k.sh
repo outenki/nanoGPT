c4ff155f7f5e88e43bea0b98d7c5b720c0241d87
outenki <outenki@gmail.com>
2025-07-24 16:05:58 +0900

    add evaluate script

---
+++ agreement_evaluation_10k.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for model_path in nonce_10k wikitext_10k; do
+    echo "========== Evaluating model: $model_path ============="
+    uv run python agreement_evaluation.py \
+        --model-path data/"$model_path"/ckpt.pt \
+        --val-data data/evaluate_data/agreement_evaluate_data.json \
+        -o data/"$model_path"
+done
