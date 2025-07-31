d313d1e53d821e6ca27d2ce9329e847c6122a115
outenki <outenki@gmail.com>
2025-07-31 15:35:32 +0900

    update job script

--- job_train_nonce_10k.sh
+++ job_train_nonce_10k.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#PJM -L "rscgrp=b-inter"
+#PJM -L "rscgrp=b-batch"
 #PJM -L "elapse=1:00:00"
-#PJM -L "gpu=3"
+#PJM -L "gpu=1"
 #PJM -L "jobenv=singularity"
 #PJM -e /home/pj25000107/ku50001566/projects/nanoGPT/logs/job_generate_nonce_data.err
 #PJM -o /home/pj25000107/ku50001566/projects/nanoGPT/logs/job_generate_nonce_data.out
-#PJM -N "singularityjob"
+#PJM -N "train_nonce_10k"
 
 module load singularity-ce
 
 cd /home/pj25000107/ku50001566/projects/nanoGPT || exit 1
 
-singularity run --nv /home/pj25000107/ku50001566/nlp-singularity/nlp-singularity.sif "/bin/bash train_nonce_10k.sh"
+singularity exec /home/pj25000107/ku50001566/nlp-singularity/nlp-singularity.sif /bin/bash train_nonce_10k.sh
