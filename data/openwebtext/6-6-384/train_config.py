de85c7f0a2f68648b24a81d2f59d5814dcfdb9e5
outenki <outenki@gmail.com>
2025-08-01 12:35:31 +0900

    update training config for openwebtext 6-6-384

--- data/openwebtext/6-6-384/train_config.py
+++ data/openwebtext/6-6-384/train_config.py
@@ -19,7 +19,24 @@ batch_size = 64
 block_size = 256  # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 12
-n_head = 12
-n_embd = 768
-dropout = 0.0
+n_layer = 6
+n_head = 6
+n_embd = 384
+dropout = 0.2
+
+learning_rate = 1e-3  # with baby networks can afford to go a bit higher
+max_iters = 5000
+lr_decay_iters = 5000  # make equal to max_iters usually
+min_lr = 1e-4  # learning_rate / 10 usually
+beta2 = 0.99  # make a bit bigger because number of tokens per iter is small
+
+warmup_iters = 100  # not super necessary potentially
+
+# on macbook also add
+# device = 'cpu'  # run on cpu only
+# compile = False # do not torch compile the model
+
+# For m-macbooks, you might want to use:
+#    python train.py config/train_nonce.py --device=mps --compile=False
+# For other devices, you can use:
+#    python train.py config/train_nonce.py --device=cuda
