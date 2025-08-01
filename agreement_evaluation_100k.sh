06fa9d61bb5407fc06368a9efe6a7520e35e53db
outenki <outenki@gmail.com>
2025-08-01 10:24:20 +0900

    fix evaluate scripts

--- agreement_evaluation_100k.sh
+++ agreement_evaluation_100k.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for model_path in nonce_100k wikitext_100k; do
-    for cfg in 6-6-384 6-6-768; do
+    for cfg in 6-6-384 12-12-768; do
         echo "========== Evaluating model: $model_path/$cfg ============="
         uv run python agreement_evaluation.py \
             --model-path data/"$model_path"/$cfg/ckpt.pt \
