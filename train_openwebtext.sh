db22ecbd9b0a52109e208ec45c6192e39e8a3701
outenki <outenki@gmail.com>
2025-08-01 12:32:20 +0900

    update training scripts for openwebtext

--- train_openwebtext.sh
+++ train_openwebtext.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-/home/pj25000107/ku50001566/.local/bin/uv run python train.py data/openwebtext/train_config.py --device=cuda > data/openwebtext/train.loss
+for cfg in 6-6-384 12-12-768; do
+    echo "Training with config: $cfg"
+    /home/pj25000107/ku50001566/.local/bin/uv run python train.py data/openwebtext/$cfg/train_config.py --device=cuda > data/openwebtext/$cfg/train.loss
+done
