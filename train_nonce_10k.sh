ce0128bda3e2f70ecdf8cc2ecc59e40340eb627b
outenki <outenki@gmail.com>
2025-08-01 09:30:49 +0900

    fix training scripts

--- train_nonce_10k.sh
+++ train_nonce_10k.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for cfg in 6-6-384 6-6-768; do
+for cfg in 6-6-384 12-12-768; do
     echo "Training with config: $cfg"
     uv run python train.py data/nonce_10k/$cfg/train_config.py --device=cuda > data/nonce_10k/$cfg/train.loss
 done
