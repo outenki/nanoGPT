22464f7189b3c8cb5930ca4a5d5e37b97ac556bf
outenki <outenki@gmail.com>
2025-08-04 13:46:00 +0900

    update train_configs

--- data/nonce_100k/12-12-768/train_config.py
+++ data/nonce_100k/12-12-768/train_config.py
@@ -1,7 +1,7 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'data/12-12-768/nonce_100k'  # <-- change to your output directory
+out_dir = 'data/nonce_100k/12-12-768'  # <-- change to your output directory
 eval_interval = 250  # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10  # don't print too too often
